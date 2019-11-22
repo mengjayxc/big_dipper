0a9cff5ce1864a961ca6677a35727a40b31cc964
mengjay315 <gaomengjie469@163.com>
2019-11-22 14:51:33 +0800

    update chain-name

--- imports/ui/proposals/Proposal.jsx
+++ imports/ui/proposals/Proposal.jsx
@@ -273,7 +273,7 @@ export default class Proposal extends Component{
                 let totalVotingPower = this.props.chain.activeVotingPower * Meteor.settings.public.stakingFraction;
                 return <div>
                     <Helmet>
-                        <title>{this.props.proposal.content.value.title} | The Big Dipper</title>
+                        <title>{this.props.proposal.content.value.title} | The ZChain Explorer</title>
                         <meta name="description" content={this.props.proposal.content.value.description} />
                     </Helmet>
 
